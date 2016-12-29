e8d258d617882190528dd011675b89891490ed94
bleurubin <bleurubin@gmail.com>
2016-12-28 17:41:39 -0800

    add @Mock

--- src/test/groovy/testapp/FooSpec.groovy
+++ src/test/groovy/testapp/FooSpec.groovy
@@ -1,5 +1,6 @@
 package testapp
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Foo)
+@Mock(Foo)
 class FooSpec extends Specification {
 
     def setup() {
